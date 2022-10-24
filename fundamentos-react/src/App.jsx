7514d9b4c5e71fd73c505a5244637d264a78c7f6
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-24 12:19:36 -0300

    23. Componente Card #02, Organizing CSS of cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,5 @@
+import './App.css'
 import React from 'react';
-
 import Card from './components/layout/Card';
 import Aletorio from './components/basicos/Aletorio';
 import Primeiro from './components/basicos/Primeiro'
@@ -8,29 +8,32 @@ import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aletorio'
 
 export default () => (
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
-        
-        <Card titulo='#04 - Exemplo de Card'>
-            <Aleatorio min={1} max={60} ></Aleatorio>
-        </Card>
 
-        <Card titulo='#03 - Fragmento'>
-            <Fragmento/>
-        </Card>
+        <div className='Cards'>
 
-        <Card titulo='#02 - Fragmento'>
-            <ComParametro
-                titulo="Situação do Aluno"
-                subtitulo="Pedro Silva" 
-                nota={8.5} 
-            />
-        </Card>
-        
-        <Card titulo='#01 - Primeiro Componente'>
-            <Primeiro></Primeiro>
-        </Card>
+            <Card titulo='#04 - Exemplo de Card'>
+                <Aleatorio min={1} max={60} ></Aleatorio>
+            </Card>
 
-        
+            <Card titulo='#03 - Fragmento'>
+                <Fragmento/>
+            </Card>
+
+            <Card titulo='#02 - Fragmento'>
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    subtitulo="Pedro Silva" 
+                    nota={8.5} 
+                />
+            </Card>
+            
+            <Card titulo='#01 - Primeiro Componente'>
+                <Primeiro></Primeiro>
+            </Card>
+
+        </div>
+                        
     </div>
 );
