7bd2fc782d5448ded1847a388f9b5d30780b30bc
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-24 09:33:31 -0300

    15. Primeiro Componente #02, Created and import first component

---
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -0,0 +1,10 @@
+export default function Primeiro (){
+    const msg = 'Seja bem vindo(a)!';
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <h2>{ msg }</h2>
+        </div>
+    );
+
+}
