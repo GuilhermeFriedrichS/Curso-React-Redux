ef36149c0bce9a97a435b98b3c60ad376d016611
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-24 11:07:48 -0300

    21. Desafio Número Aleatório - Resposta, Created component that generate a random number

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import Aletorio from './components/basicos/Aletorio';
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from './components/basicos/Aletorio'
 
 export default () => (
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Aleatorio min={1} max={60} ></Aleatorio>
         <Fragmento/>
         <ComParametro
             titulo="Situação do Aluno"
---
+++ fundamentos-react/src/components/basicos/Aletorio.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default props => {
+    const { min, max } = props;
+    const aleatorio = parseInt(Math.random() * (max-min)) + min;
+
+    return (
+        <div>
+            <h2>Valor Aleatório</h2>
+            <p>
+                <strong>Valor Mínimo: </strong> {props.min}
+            </p>
+            <p>
+                <strong>Valor Máximo: </strong> {props.max}
+            </p>
+            <p>
+                <strong>Valor escolhido: </strong> {aleatorio}
+            </p>
+        </div>
+    );
+
+};
