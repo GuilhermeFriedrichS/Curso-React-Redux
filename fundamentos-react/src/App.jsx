425cc878bab5e46a789e8a320e1c081817e157d3
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-24 12:02:23 -0300

    22. Componente Card #01, Created Card component to display exercices inside of Card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Card from './components/layout/Card';
 import Aletorio from './components/basicos/Aletorio';
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
@@ -9,13 +10,27 @@ import Aleatorio from './components/basicos/Aletorio'
 export default () => (
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={1} max={60} ></Aleatorio>
-        <Fragmento/>
-        <ComParametro
-            titulo="Situação do Aluno"
-            subtitulo="Pedro Silva" 
-            nota={8.5} 
-        />
-        <Primeiro></Primeiro>
+        
+        <Card titulo='#04 - Exemplo de Card'>
+            <Aleatorio min={1} max={60} ></Aleatorio>
+        </Card>
+
+        <Card titulo='#03 - Fragmento'>
+            <Fragmento/>
+        </Card>
+
+        <Card titulo='#02 - Fragmento'>
+            <ComParametro
+                titulo="Situação do Aluno"
+                subtitulo="Pedro Silva" 
+                nota={8.5} 
+            />
+        </Card>
+        
+        <Card titulo='#01 - Primeiro Componente'>
+            <Primeiro></Primeiro>
+        </Card>
+
+        
     </div>
 );
