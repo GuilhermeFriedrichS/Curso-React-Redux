337d28c77e3d5f68303756652b82376483224d96
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-10-26 08:19:42 -0300

    35. Componente com Estado, Using useState to change and display changes of variables

--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import IndiretaFilho from "./IndiretaFilho";
 
 export default props => {
+    const [a, b] = [1, 2]
+    const [nome, setNome] = useState('?');
+    const [idade, setIdade] = useState(0);
+    const [nerd, setNerd] = useState(false);
 
-    let nome = '?'
-    let idade = 0
-    let nerd = false
-
-    function fornecerInformacoes(nomeParam, idadeParam, nerdParam){
-        nome = nomeParam
-        idade = idadeParam
-        nerd = nerdParam
-
-        console.log(nomeParam, idadeParam, nerdParam)
-        console.log(nome, idade, nerd)
+    function fornecerInformacoes(nome, idade, nerd){
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
     }
 
     return (
         <div>
             <div>
-                <span>{nome}</span>
+                <span>{nome} </span>
                 <span><strong>{idade}</strong></span>
-                <span>{nerd ? 'Verdadeiro' : 'Falso'}</span>
+                <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
             </div>    
             <IndiretaFilho quandoClicar={fornecerInformacoes} ></IndiretaFilho>
         </div>
