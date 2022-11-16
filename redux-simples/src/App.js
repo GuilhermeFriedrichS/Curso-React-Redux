d313201fc93de9339aa732e65a93404d7c5f20b1
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-16 10:38:02 -0300

    163. Limpando a Implementação, Cleaning up implementation to use Redux

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 import Interval from './components/Interval';
@@ -8,20 +8,16 @@ import DrawLots from './components/DrawLots';
 
 function App() {
 
-  const [min, setMin] = useState(10)
-  const [max, setMax] = useState(20)
-
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Interval min={min} max={max}
-          onMinChanged={setMin} onMaxChanged={setMax}></Interval>
+        <Interval></Interval>
       </div>
       <div className="linha">
-        <Avarage min={min} max={max}></Avarage>
-        <Sum min={min} max={max}></Sum>
-        <DrawLots min={min} max={max}></DrawLots>
+        <Avarage></Avarage>
+        <Sum></Sum>
+        <DrawLots></DrawLots>
       </div>  
     </div>
   );
--- redux-simples/src/components/Sum.jsx
+++ redux-simples/src/components/Sum.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
-    const { min, max } = props
     return (
         <Card title="Soma de Números" blue>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>{min+max}</strong>
+                    <strong>10</strong>
                 </span>
             </div>
         </Card>
