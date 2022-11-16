103c539c9768a306f8d8ad264ea1b7875b641ed5
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-16 09:26:47 -0300

    159. Componente Intervalo, Created compontent with inputs

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Interval from './components/Interval';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Card title="Card 1" red>X</Card>
-        <Card title="Card 2" green>X</Card>
+        <Interval></Interval>
       </div>
       <div className="linha">
+        <Card title="Card 2" green>X</Card>
         <Card title="Card 3" blue>X</Card>
         <Card title="Card 4" purple>X</Card>
       </div>  
