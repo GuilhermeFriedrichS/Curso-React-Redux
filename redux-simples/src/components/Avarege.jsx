5a320392e1be0c321edff35d878cf86804cd5166
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-16 12:08:16 -0300

    165. Conectando Componentes com Redux #01, Using Redux to connect components

--- redux-simples/src/components/Avarege.jsx
+++ redux-simples/src/components/Avarege.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import Card from './Card'
 
-export default props => {
+function Media (props) {
+    console.log(props)
+    const {min, max} = props
     return (
         <Card title="Média de Números" green>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>{10}</strong>
+                    <strong>{(max + min) / 2}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        min: state.numbers.min,
+        max: state.numbers.max,
+    }
+}
+
+export default connect(mapStateToProps)(Media)
