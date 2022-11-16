258d3deba3dbb5a6db4b801adecf52c3d335339c
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-16 10:29:36 -0300

    162. Implementação SEM Redux, Using useState to change min and max numbers

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 import Interval from './components/Interval';
@@ -7,16 +7,21 @@ import Sum from './components/Sum';
 import DrawLots from './components/DrawLots';
 
 function App() {
+
+  const [min, setMin] = useState(10)
+  const [max, setMax] = useState(20)
+
   return (
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className="linha">
-        <Interval></Interval>
+        <Interval min={min} max={max}
+          onMinChanged={setMin} onMaxChanged={setMax}></Interval>
       </div>
       <div className="linha">
-        <Avarage></Avarage>
-        <Sum></Sum>
-        <DrawLots></DrawLots>
+        <Avarage min={min} max={max}></Avarage>
+        <Sum min={min} max={max}></Sum>
+        <DrawLots min={min} max={max}></DrawLots>
       </div>  
     </div>
   );
--- redux-simples/src/components/DrawLots.jsx
+++ redux-simples/src/components/DrawLots.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    const { min, max } = props
+    const random = parseInt(Math.random() * (max - min)) + min
     return (
         <Card title="Sorteio de um Números" purple>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>8</strong>
+                    <strong>{random}</strong>
                 </span>
             </div>
         </Card>
