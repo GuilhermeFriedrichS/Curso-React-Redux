ae00c94d90bc59586622592d511b3cc74d9e1dbc
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-17 09:00:15 -0300

    171. Constantes para os Tipos das Actions, Created constants that receive the types of actions

--- redux-simples/src/store/actions/numbers.js
+++ redux-simples/src/store/actions/numbers.js
@@ -1,14 +1,19 @@
+import {
+    NUM_MIN_CHANGED,
+    NUM_MAX_CHANGED
+} from './actionsTypes'
+
 // Action Creator
 export function changeNumberMin (newNumber) {
     return {
-        type: 'NUM_MIN_CHANGED',
+        type: NUM_MIN_CHANGED,
         payload: newNumber
     }
 }
 
 export function changeNumberMax (newNumber) {
     return {
-        type: 'NUM_MAX_CHANGED',
+        type: NUM_MAX_CHANGED,
         payload: newNumber
     }
 }
--- redux-simples/src/store/reducers/numbers.js
+++ redux-simples/src/store/reducers/numbers.js
@@ -1,3 +1,8 @@
+import {
+    NUM_MIN_CHANGED,
+    NUM_MAX_CHANGED
+} from '../actions/actionsTypes'
+
 const initialState = {
     min: 1,
     max: 10
@@ -6,12 +11,12 @@ const initialState = {
 export default function(state = initialState, action) {
 
     switch(action.type){
-        case 'NUM_MIN_CHANGED':
+        case NUM_MIN_CHANGED:
             return{
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_CHANGED':
+        case NUM_MAX_CHANGED:
             return{
                 ...state,
                 max: action.payload
