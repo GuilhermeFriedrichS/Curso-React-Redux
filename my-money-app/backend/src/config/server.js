66e33e4f3a3872f1fbb5b049c006c3b4cb41f5d3
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-21 07:53:07 -0300

    196. Mapeamento das Rotas, Mapping Api routes

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,2 +1,14 @@
-require ('./config/server')
-require ('.config/database')
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
