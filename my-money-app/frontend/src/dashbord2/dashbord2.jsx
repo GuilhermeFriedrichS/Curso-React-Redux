9b37713d845b01ee910a66cc44d6b8f5c18d0083
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-22 11:29:31 -0300

    229. Componentes Estruturais das Abas, Created components to create tabs menu

--- my-money-app/frontend/src/dashbord2/dashbord2.jsx
+++ my-money-app/frontend/src/dashbord2/dashbord2.jsx
@@ -5,6 +5,7 @@ import ContentHeader from "../common/template/contentHeader"
 import Content from "../common/template/content"
 import ValueBox from "../common/widget/valueBox"
 import Row from "../common/layout/row"
+import Tabs from "../common/tab/tabs"
 
 const BASE_URL = 'http://localhost:3003/api'
 
