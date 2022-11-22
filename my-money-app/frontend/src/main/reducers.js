b85ab472df23a0eba794a1b41852e05853b0ca4d
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-22 12:33:03 -0300

    231. Criando a Primeira Action e o Reducer, Created Reducer and Action Creator of tab ID

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux'
+
 import DashbordReducer from '../dashbord/dashbordReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
-    dashbord: DashbordReducer
+    dashbord: DashbordReducer,
+    tab: TabReducer
 })
 
 export default rootReducer 
