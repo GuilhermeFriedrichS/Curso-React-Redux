0c4738c1f33888eaeb27dc60b44ee89e02ad7238
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-23 09:35:42 -0300

    238. Criando a Action getList e o Reducer, Created Action and Reducer to get list from MongoDB

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import DashbordReducer from '../dashbord/dashbordReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashbord: DashbordReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    BillingCycle: BillingCycleReducer
 })
 
 export default rootReducer 
