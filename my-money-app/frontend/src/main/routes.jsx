19b604d8cb2cd2a550decd01ceb5a09ba1e1c4cd
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-25 10:29:38 -0300

    263. Navegação com React-router (Versão 2), Refactoring code to import App in routes and use IndexRoute

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -1,13 +1,16 @@
 import React from "react"
-import { Router, Route, Redirect, hashHistory} from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory} from 'react-router'
 
+import App from './app'
 import Dashbord from "../dashbord2/dashbord2"
 import BillingCycle from "../billingCycle/billingCycle"
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashbord}/>
-        <Route path='/billingCycles' component={BillingCycle}/>
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashbord}/>
+        <Route path='billingCycles' component={BillingCycle}/>
+        </Route>
         <Redirect from='*' to='/'/>
     </Router>
 )
