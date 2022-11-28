b2065b33b790580196d97e413bdae886d2ee850f
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-28 11:19:40 -0300

    282. Parte #01, Updating dependencies versions

---
+++ my-money-app/frontend-old/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022
+            <a href="https://localhost:8080/footer" target='_blank'> Footer</a>.
+        </strong>
+    </footer>
+)
