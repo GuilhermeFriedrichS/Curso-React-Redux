9f65c5e7a6c1ff50c17c3591ea47f8e14c33e4f6
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-11-30 11:32:25 -0300

    308. Express - Exercício 03: Método USE, Mapping routes using USE

--- exercicios-express/ex03.js
+++ exercicios-express/ex03.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const server = express()
-//.get usa quando tem que ser a url exata
-server.get('/', function(req, res, next) {
+//.use usa quando quer que inicie com aquela URL
+server.use(function(req, res, next) {
     console.log('Inicio!')
     next()
     console.log('Fim!')
 })
 
-server.get('/', function(req, res) {
+server.use(function(req, res) {
     console.log('Resposta!')
     res.send('<h1>Teste Express!</h1')
 })
