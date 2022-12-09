ababf9e0cc07391732dfe56ebda3ae04dd1ce5e0
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-12-09 09:05:52 -0300

    393. Adicionando o Loader para CSS, Installing and configuring CSS Loader

--- exercicio-webpack-legado/webpack.config.js
+++ exercicio-webpack-legado/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js', //Arquivo de entrada que vai referenciar todo o resto
@@ -10,6 +11,9 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    plugins: [
+        new ExtractTextPlugin('app.css') //Arquivo saida do CSS
+    ],
     module: {
         loaders: [{//Configura loader para que os import e exports funcionem
             test: /.js?/,
@@ -19,6 +23,9 @@ module.exports = {
                 presets: ['es2015', 'react'], //Para entender os export e imports
                 plugins: ['transform-object-rest-spread'] //Para entender o spread (...)
             }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader") //Carrega CSS
         }]
     }
 }
