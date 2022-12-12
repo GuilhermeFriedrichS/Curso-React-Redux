aa7de02c1ed31a4e1de0452d2252a7505932db4d
GuilhermeFriedrichS <guilhermefriedrichsousa@gmail.com>
2022-12-12 10:08:05 -0300

    436. Palavras Iniciais e Instalação, Creating project

--- exercicio-webpack-legado/webpack.config.js
+++ exercicio-webpack-legado/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './ex/index.js', //Arquivo de entrada que vai referenciar todo o resto
+    entry: './ex/index.js', //Arquivo de entrada que vai referenciar todo o resto, No caso do react no lugar do js ia ser jsx
     output: {
         path: __dirname + '/public',
         filename: './bundle.js' //Arquivo de saida que existe em tempo de execução a não ser que gere um com "./node_modules/.bin/webpack"
